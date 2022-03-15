c78498dbbb24cadd152b055ba1b44db6490c7232
Linus <70134500+ItzLinus@users.noreply.github.com>
2022-03-15 16:26:02 +0100

    init

---
+++ routes/users.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router;
+
+const users = [{name: 'Linus', email: 'linus@mail.com'}]
+
+router.get('/users', (_, res) => {
+    res.json({ ok: true, users });
+});
+
+router.get('/user/:name', (req, res) => {
+    const { name } = req.params;
+    const user = users.filter((user) => user.name === name)[0];
+    res.json({ ok: true, user });
+});
+
+
+router.post('/adduser', (req, res) => {
+    const { name, email } = req.body;
+    if (name && email) {
+        users.push({ name, email});
+        res.json({ ok: true, users });
+    } 
+});
+
+module.exports = router;
