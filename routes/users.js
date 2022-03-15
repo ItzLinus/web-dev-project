3ce785aaaa861efa04204494ec2906b6b2906fa2
Linus <70134500+ItzLinus@users.noreply.github.com>
2022-03-15 19:42:02 +0100

    init

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
-const router = express.Router;
+import { Router } from 'express';
+
+const router = Router();
 
 const users = [{name: 'Linus', email: 'linus@mail.com'}]
 
